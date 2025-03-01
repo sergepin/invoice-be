d1ac7dbd26587435c3fa467d6da24bedbf7d9a83
Sergio Pinzon <sergiopin596@gmail.com>
2025-03-01 01:20:31 -0500

    update getorders endpoint

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,12 +4,14 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { Order, OrderSchema } from '../schemas/orders.schema';
 import { User, UserSchema } from '../schemas/user.schema';
+import { Product, ProductSchema } from 'src/schemas/products.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Order.name, schema: OrderSchema },
       { name: User.name, schema: UserSchema },
+      { name: Product.name, schema: ProductSchema },
     ]),
   ],
   controllers: [OrdersController],
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -4,12 +4,14 @@ import { Model } from 'mongoose';
 import { Order, OrderDocument } from '../schemas/orders.schema';
 import { User, UserDocument } from '../schemas/user.schema';
 import * as moment from 'moment';
+import { Product, ProductDocument } from 'src/schemas/products.schema';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectModel(Order.name) private orderModel: Model<OrderDocument>,
     @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
   ) {}
 
   async getAllUsersPurchasesLastMonth() {
@@ -35,7 +37,9 @@ export class OrdersService {
     const users = await this.userModel.find({ _id: { $in: userIds } });
 
     return users.map((user) => {
-      const purchaseData = purchases.find((p) => p._id.toString() === user._id.toString());
+      const purchaseData = purchases.find(
+        (p) => p._id.toString() === user._id.toString(),
+      );
       return {
         userId: user._id.toString(),
         name: user.name,
@@ -55,13 +59,31 @@ export class OrdersService {
         'You do not have permission to view these orders',
       );
     }
-
     const oneMonthAgo = moment().subtract(30, 'days').toDate();
-
     const orders = await this.orderModel.find({
       user_id: targetUserId,
       status: 'completed',
       date: { $gte: oneMonthAgo },
+    });
+
+    const productIds = [
+      ...new Set(
+        orders.flatMap((order) =>
+          order.products.map((p) => p.product_id.toString()),
+        ),
+      ),
+    ];
+
+    const products = await this.productModel.find({
+      _id: { $in: productIds },
+    });
+
+    const productMap = new Map();
+    products.forEach((product) => {
+      productMap.set(product.id.toString(), {
+        name: product.name,
+        price: product.price,
+      });
     });
 
     return orders.map((order) => ({
@@ -70,7 +92,11 @@ export class OrdersService {
       totalAmount: order.totalAmount,
       date: order.date,
       products: order.products.map((p) => ({
-        productId: p.product_id,
+        productId: p.product_id.toString(),
+        name:
+          productMap.get(p.product_id.toString())?.name ||
+          'Producto no encontrado',
+        unitPrice: productMap.get(p.product_id.toString())?.price || 0,
         quantity: p.quantity,
       })),
     }));
